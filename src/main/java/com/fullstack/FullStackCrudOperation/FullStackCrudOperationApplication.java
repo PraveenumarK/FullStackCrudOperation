fdc309e93c571a58aafe6b32beb38ce8dd099b40
PraveenumarK <kprave05@gmail.com>
2024-12-27 10:11:48 +0530

    commit project files

---
+++ src/main/java/com/fullstack/FullStackCrudOperation/FullStackCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.FullStackCrudOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackCrudOperationApplication.class, args);
+	}
+
+}
